9bc3e0f1bb61cc152be8dc8b9d6cc1619e33e436
KaushalDhakal0 <kaushaldhakal90@gmail.com>
2023-03-31 14:30:17 +0545

    Check if Prime

---
+++ src/Prime.java
@@ -0,0 +1,20 @@
+public class Prime {
+    static boolean isPrime( int num){
+        boolean ans = true;
+        if (num <= 2){
+            return true;
+        }
+        for (int i = 2 ; i< num/2; i++){
+            if(num%i == 0){
+                return false;
+            }
+        }
+        return ans;
+    }
+     public static void main(String[] args){
+        int num = 989;
+
+        System.out.println("IS PRIME = "+  isPrime(num)  );
+    }
+
+}
