9833d3fe4d35ecf8fe8fb5f7a29d7ff18db54ed6
KaushalDhakal0 <kaushaldhakal90@gmail.com>
2023-03-31 15:16:15 +0545

    Bug Fix

--- src/com/kaushal/Main.java
+++ src/com/kaushal/Main.java
@@ -4,21 +4,24 @@ import java.util.Scanner;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     static boolean isPrime( int num){
+        System.out.println("====="+ num);
         boolean ans = true;
         if (num <= 2){
             return true;
         }
-        for (int i = 2 ; i< num/2; i++){
-            if(num%i == 0){
+        for (int i = 2 ; i<= num/2; i++){
+            if(num % i == 0){
                 return false;
             }
         }
-        return ans;
+        return true;
     }
     public static void main(String[] args) {
         Scanner dta =  new Scanner(System.in);
-        System.out.println("Enter a number to check if Prime:=");
-        int inputNum =  dta.nextInt();
-        System.out.println("IS PRIME =" + isPrime(inputNum));
+        for (int i = 0 ; i < 10; i++){
+            System.out.println("Enter a number to check if Prime:=");
+            int inputNum =  dta.nextInt();
+            System.out.println("IS PRIME =" + isPrime(inputNum));
+        }
     }
 }
