2eb3922a29602eccc5567152050dad9e1f7fe507
KaushalDhakal0 <kaushaldhakal90@gmail.com>
2023-04-01 20:11:44 +0545

    Leetcode 27.Remove Element

--- src/com/kaushal/Functions.java
+++ src/com/kaushal/Functions.java
@@ -1,7 +1,28 @@
 package com.kaushal;
+//LINK => https://leetcode.com/problems/remove-element/
+//Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+//
+//        Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+//
+//        Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important as well as the size of nums.
+//        Return k.
 
 public class Functions {
     public static void main(String[] args) {
-
+        int [] x  = {3,2,2,3,4,5,6,7,8,90,1,2,3,4,5,6,7,8};
+        int k = 3;
+        int ans  =  removeElement(x,k);
+        System.out.println("====ANS=====" + ans);
     }
-}
+    static int  removeElement(int [] nums, int val) {
+        int k = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+    }
+
--- src/com/kaushal/SwitchCase.java
+++ src/com/kaushal/SwitchCase.java
@@ -1,8 +1,9 @@
 package com.kaushal;
 
+import java.util.Scanner;
+
 public class SwitchCase {
     public static void main(String[] args) {
-        public static void main(String[] args) {
             Scanner in = new Scanner(System.in);
             int empID = in.nextInt();
             String department = in.next();
