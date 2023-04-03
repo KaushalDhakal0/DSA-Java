627f00339aa60ad7d2f4a51b00bcaaa482a2cd7a
KaushalDhakal0 <kaushaldhakal90@gmail.com>
2023-04-03 14:51:31 +0545

    Arrays Started

---
+++ src/com/kaushal/ArraysAndArrayList.java
@@ -0,0 +1,10 @@
+package com.kaushal;
+
+import java.util.Arrays;
+
+public class ArraysAndArrayList {
+    public static void main(String[] args) {
+         int[] array = {1,2,3,4,5,6,7,8,9};
+        System.out.println(Array.);
+    }
+}
