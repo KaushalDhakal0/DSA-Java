bbe97c50fa2dd0ca701c382562c8c487d3ff81db
KaushalDhakal0 <kaushaldhakal90@gmail.com>
2023-04-03 15:50:16 +0545

    Array List

--- src/com/kaushal/ArraysAndArrayList.java
+++ src/com/kaushal/ArraysAndArrayList.java
@@ -1,24 +1,24 @@
 package com.kaushal;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class ArraysAndArrayList {
     public static void main(String[] args) {
-         int[] array = {123,78,54,67,0,1,2,3,4,5,6,7,8,900};
+//         int[] array = {123,78,54,67,0,1,2,3,4,5,6,7,8,900};
 //         System.out.println("Input Array ==>" + Arrays.toString(array));
 //        System.out.println("Length =>" + array.length);
 //        //Arrays.sort(arr) modifies the original array;
-////        Arrays.sort(array);
+//        Arrays.sort(array);
 //        newSort(array);
 //        System.out.println("Sorting Array " +  Arrays.toString(array));
-//        //Searching in array
-//        int index = Arrays.binarySearch(array, 7);
-//        System.out.println("Binary search ==" + index + "Value ===" + array[index]);
-
-        //Find Largest
-        finLargestInArray(array);
-        System.out.println("Largest Element in the array = " + array[array.length - 1]);
-        System.out.println("Largest Element in the array  second approach= " + findLargest(array));
+//        //Searching in array. To use binary search array must be sorted.
+//        int index = Arrays.binarySearch(array, 987);
+//        System.out.println("Binary search ==::" + index );
+        int[] array = {1, 2, 3, 4, 5};
+        IntStream stream = Arrays.stream(array);
     }
     static int[] newSort(int[] val){
         // If you don't clone the array Original array will be modified.
