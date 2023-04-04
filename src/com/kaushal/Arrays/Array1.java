786d6149d22574cefad94db5ccec9c86c2e4b5a8
KaushalDhakal0 <kaushaldhakal90@gmail.com>
2023-04-04 20:08:01 +0545

    Looping array Compile Time Vs RunTime

---
+++ src/com/kaushal/Arrays/Array1.java
@@ -0,0 +1,24 @@
+package com.kaushal.Arrays;
+
+import java.util.Arrays;
+
+public class Array1 {
+    public static void main(String[] args) {
+        //Primitive Data Types are Passed By Values but Non Primitive Data Types are passed by Reference.
+        int[] listArr = {1,2,3,4,5,6,7};
+        //RunTime Array
+        int[] runtimeArray = new int[10];
+        Arrays.fill(runtimeArray, 98);
+        String[] strArray =  new String[10];
+        Arrays.fill(strArray, "KD");
+        //Printing Arrays
+        System.out.println("String Array =" + Arrays.toString(strArray));
+        System.out.println("ARRay ==" + Arrays.toString(runtimeArray));
+
+        //Using Loops
+        for (int i = 0; i < listArr.length; i++) {
+            System.out.println(strArray[i] + " str");
+            System.out.println(listArr[i] + " ");
+        }
+    }
+}
