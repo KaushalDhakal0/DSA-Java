4881ab9f40bdb8a6cfa44d414c7e91fcc33cf80f
KaushalDhakal0 <kaushaldhakal90@gmail.com>
2023-04-07 00:39:34 +0545

    Find First and Last Position of Element in Sorted Array:LeetCode:34

---
+++ src/com/kaushal/BinarySearch/FirstAndLastPosition.java
@@ -0,0 +1,45 @@
+package com.kaushal.BinarySearch;
+
+import java.util.Arrays;
+
+public class FirstAndLastPosition {
+    public static void main(String[] args) {
+        int[] arr = {5,7,7,8,8,10};
+        int target = 8;
+        int[] result = getAns(arr,target);
+        System.out.println("Hello");
+        System.out.println("Answer == " + Arrays.toString(result));
+    }
+
+    private static int[] getAns(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        int[] ans = {-1,-1};
+        while (left <= right){
+            int mid = left  + (right-left)/2;
+            if (target > nums[mid]){
+                left = mid + 1 ;
+            } else if (target < nums[mid]) {
+                right = mid - 1;
+            }else {
+                ans[0] = mid;
+                right = mid -1;
+            }
+        }
+
+         left = 0;
+         right = nums.length - 1;
+        while (left <= right){
+            int mid = left  + (right-left)/2;
+            if (target > nums[mid]){
+                left = mid + 1 ;
+            } else if (target < nums[mid]) {
+                right = mid - 1;
+            }else {
+                ans[1] = mid;
+                left = mid + 1;
+            }
+        }
+        return ans;
+    }
+}
---
+++ src/com/kaushal/BinarySearch/OptimizedFirstAndLastPosition.java
@@ -0,0 +1,53 @@
+package com.kaushal.BinarySearch;
+
+import java.util.Arrays;
+
+public class OptimizedFirstAndLastPosition {
+    public static void main(String[] args) {
+        int[] arr = {5,7,7,8,8,10};
+        int target = 104;
+        int[] ans = {-1, -1};
+        int leftBoundary = findLeftBoundary(arr,target);
+        int rightBoundary = findRightBoundary(arr,target);
+        ans[0] = leftBoundary;
+        ans[1] = rightBoundary;
+        System.out.println("Ans == " + Arrays.toString(ans));
+    }
+
+    private static int findRightBoundary(int[] arr, int target) {
+            int rightBoundary = -1;
+            int left = 0 ;
+            int right = arr.length - 1;
+            while (left <= right){
+                int mid = left + (right - left)/2;
+                if (arr[mid] == target){
+                    rightBoundary = mid;
+                    left = mid + 1;
+                }else if(arr[mid] > target){
+                    right = mid - 1;
+                }else {
+                    left = mid + 1;
+                }
+            }
+            return rightBoundary;
+
+    }
+
+    private static int findLeftBoundary(int[] arr, int target) {
+        int leftBoundary = -1;
+        int left = 0 ;
+        int right = arr.length - 1;
+        while (left <= right){
+            int mid = left + (right - left)/2;
+            if (arr[mid] == target){
+                leftBoundary = mid;
+                right = mid - 1;
+            }else if(arr[mid] > target){
+                right = mid - 1;
+            }else {
+                left = mid + 1;
+            }
+        }
+        return leftBoundary;
+    }
+}
