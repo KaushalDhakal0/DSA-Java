ee18d1ce628620efae4da922a1186338bb8e9d96
KaushalDhakal0 <kaushaldhakal90@gmail.com>
2023-04-21 21:20:25 +0545

    Adding checks for Already sorted array

--- src/com/kaushal/SortingAlgorithms/Bubble.java
+++ src/com/kaushal/SortingAlgorithms/Bubble.java
@@ -4,19 +4,26 @@ import java.util.Arrays;
 
 public class Bubble {
     public static void main(String[] args) {
-        int[] arr = {12,32, 1,2,8,5,3,9,7};
+//        int[] arr = {12,32, 1,2,9,79,8,5,3,9,79};
+        int[] arr = {1,2,3,4,5,6,7,8};
         bubbleSort(arr);
         System.out.println(" After Sorting : " + Arrays.toString(arr));
     }
 
     private static void bubbleSort(int[] arr) {
-        for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = 0; j < arr.length - i - 1; j++) {
+        boolean isAlreadySorted = true;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - i - 1 ; j++) {
                 if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
+                    isAlreadySorted = false;
                 }
+            }
+            //If Already Sorted it goes through only one loop and breaks. If this check is not performed then outer loop will run for n-1 times.i.e redundant iteration
+            if (isAlreadySorted){
+                break;
             }
         }
     }
